d39a69de834b688068c6165818458e407dde5474
Dread <dread@pop-os.localdomain>
2022-02-15 19:36:37 -0500

    updated manifest.yaml

--- Makefile
+++ Makefile
@@ -1,20 +1,19 @@
-ASSETS := $(shell yq e '.assets.[].src' manifest.yaml)
-ASSET_PATHS := $(addprefix assets/,$(ASSETS))
-VERSION := $(shell toml get lndg/Cargo.toml package.version)
+VERSION := $(shell cat ./Dockerfile | head -n 1 | sed -e 's/^.*://')
+EMVER := $(shell yq e ".version" manifest.yaml)
 LNDG_SRC := $(shell find ./lndg)
-VERSION := $(shell yq e ".version" manifest.yaml)
+S9PK_PATH=$(shell find . -name lndg.s9pk -print)
 
 .DELETE_ON_ERROR:
 
 all: verify
 
-verify: lndg.s9pk
-	embassy-sdk verify lndg.s9pk
+verify: lndg.s9pk $(S9PK_PATH)
+	embassy-sdk verify $(S9PK_PATH)
 
-install: lndg.s9pk
+install: lndg.s9pk 
 	embassy-cli package install lndg.s9pk
 
-lndg.s9pk: manifest.yaml assets/compat/config_spec.yaml assets/compat/config_rules.yaml image.tar instructions.md
+lndg.s9pk: manifest.yaml assets/compat/* image.tar doc/instructions.md LICENSE icon.png
 	embassy-sdk pack
 
 Dockerfile: $(LNDG_SRC)
@@ -22,5 +21,4 @@ Dockerfile: $(LNDG_SRC)
 	patch -u Dockerfile -i lndg.patch
 
 image.tar: Dockerfile docker_entrypoint.sh
-	docker build --tag start9/lndg/main:$(VERSION) .
-	docker save -o image.tar start9/lndg/main:$(VERSION)
+	DOCKER_CLI_EXPERIMENTAL=enabled docker buildx build --tag start9/lndg/main:${EMVER} --platform=linux/arm64/v8 -f Dockerfile -o type=docker,dest=image.tar .
--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,5 +1,54 @@
+#!/bin/sh
+
+_term() {
+  echo "caught SIGTERM signal!"
+  kill -TERM "$lndg_child" 2>/dev/null
+}
+
 python initialize.py -net 'mainnet' -server 'localhost:10009' -d
+sleep 5
+
 python manage.py migrate
+
 python manage.py collectstatic --no-input
+
 supervisord
+
 python manage.py runserver 0.0.0.0:8889
+sleep 5
+
+render_properties() {
+    while true; do
+      # sleep 5
+      # export SERVER_BUNDLE=$( cat var/lib/cwtch/serverbundle || true )
+      if [ -z "$SERVER_BUNDLE" ];
+      then
+        # Properties 
+        echo 'version: 2' > /var/lib/cwtch/start9/stats.yaml
+        echo 'data:' >> /var/lib/cwtch/start9/stats.yaml
+        echo '  LNDg:' >> /var/lib/cwtch/start9/stats.yaml
+        echo '    type: string' >> /var/lib/cwtch/start9/stats.yaml
+        echo '    value: "'"{Placeholder}"'"' >> /var/lib/cwtch/start9/stats.yaml
+        echo '    description: This is the part you need to capture and import into a Cwtch client app so you can use the server for hosting groups' >> /var/lib/cwtch/start9/stats.yaml
+        echo '    copyable: true' >> /var/lib/cwtch/start9/stats.yaml
+        echo '    masked: false' >> /var/lib/cwtch/start9/stats.yaml
+        echo '    qr: true' >> /var/lib/cwtch/start9/stats.yaml
+      else
+        # Properties 
+        echo 'version: 2' > /var/lib/cwtch/start9/stats.yaml
+        echo 'data:' >> /var/lib/cwtch/start9/stats.yaml
+        echo '  Node Alias:' >> /var/lib/cwtch/start9/stats.yaml
+        echo '    type: string' >> /var/lib/cwtch/start9/stats.yaml
+        echo '    value: "'"$SERVER_BUNDLE"'"' >> /var/lib/cwtch/start9/stats.yaml
+        echo '    description: This is the part you need to capture and import into a Cwtch client app so you can use the server for hosting groups' >> /var/lib/cwtch/start9/stats.yaml
+        echo '    copyable: true' >> /var/lib/cwtch/start9/stats.yaml
+        echo '    masked: false' >> /var/lib/cwtch/start9/stats.yaml
+        echo '    qr: true' >> /var/lib/cwtch/start9/stats.yaml
+      fi
+    done
+}
+
+trap _term SIGTERM
+
+render_properties &
+wait -n $lndg_child 

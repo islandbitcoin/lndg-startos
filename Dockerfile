51c367476d5fd0317bad973abf19d37445629ae7
Dread <dread@pop-os.localdomain>
2022-02-20 04:25:59 -0500

    docker-compose working, cannot findfoldr

--- Dockerfile
+++ Dockerfile
@@ -3,12 +3,13 @@ ENV PYTHONDONTWRITEBYTECODE=1
 ENV PYTHONUNBUFFERED=1
 COPY . src/
 WORKDIR /src/lndg/
-RUN pip install -r requirements.txt
-RUN pip install supervisor whitenoise
+RUN pip install -r requirements.txt \
+    && pip install supervisor whitenoise
 
 # Embassy Setup for docker entrypoint
 WORKDIR /
 RUN pip install docker-compose
+ADD ./assets/compat/docker-compose.yaml /src/lndg/
 ADD ./docker_entrypoint.sh /usr/local/bin/docker_entrypoint.sh
 RUN chmod a+x /usr/local/bin/docker_entrypoint.sh
 EXPOSE 8889
--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,22 +1,7 @@
 #!/bin/sh
 set -e
 export HOST_IP=$(ip -4 route list match 0/0 | awk '{print $3}')
-echo " \n Starting service... \n"
 cd src/lndg
-echo " \n modifying docker-compose to find LND directory... \n"
-sed 's/\/root\/<user>\/.lnd/\/mnt\/lnd/' docker-compose.yaml | sed 's/\/root\/<user>\/<path-to>/\/src/'
-sed -i 's/\/root\/<user>\/.lnd/\/mnt\/lnd/' docker-compose.yaml | sed 's/\/root\/<user>\/<path-to>/\/src/'
 echo " \n Starting LNDg... \n"
-# running this will start the LNDg docker and expose the tool at port 8889
-#python initialize.py -net 'mainnet' -server '127.0.0.1:10009' -d && supervisord && python manage.py runserver 0.0.0.0:8889
-# docker-compose up -d
-cd ../..
-pwd
-echo "listing root"
-ls root
-echo "listing mnt"
-ls mnt
-echo "listing find .lnd"
-find / -name .lnd
-find / -name lnd
-/bin/sh
+# # running this will start the LNDg docker and expose the tool at port 8889
+docker-compose up -d

9822501c890838b112aa75d2329b79a7ba2e75c5
Dread <dread@pop-os.localdomain>
2022-02-19 23:42:12 -0500

    .

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -3,10 +3,10 @@ set -e
 export HOST_IP=$(ip -4 route list match 0/0 | awk '{print $3}')
 echo " \n Starting LNDg... \n"
 pwd
+echo "cd into lndg \n"
+cd src/lndg
+echo "list files in lndg \n"
 ls -al
-cd src
-ls -al
-cd lndg
-ls -al
+pwd
 # running this will start the LNDg docker and expose the tool at port 8889
 # python initialize.py -net 'mainnet' -server '127.0.0.1:10009' -d && supervisord && python manage.py runserver 0.0.0.0:8889

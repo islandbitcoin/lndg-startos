55711ac0d90d4313641bb7733a391a75a778e9e4
Dread <dread@pop-os.localdomain>
2022-02-21 15:48:38 -0500

    updated entrypoint

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,15 +1,7 @@
 #!/bin/sh
 set -e
 echo " \n Starting LNDg... \n"
-# echo "running .venv/bin/python initialize.py -net 'mainnet' -server 'localhost:10009' -d -dx -pw 'lndg-admin'"
-# .venv/bin/python initialize.py -net 'mainnet' -server 'localhost:10009' -d -dx -pw 'lndg-admin' -dir /mnt/lnd
-# echo "running .venv/bin/python manage.py migrate"
-# .venv/bin/python manage.py migrate
-# echo "running .venv/bin/python manage.py collectstatic --no-input"
-# .venv/bin/python manage.py collectstatic --no-input
-echo "running .venv/bin/pip install whitenoise tzdata && .venv/bin/python initialize.py -net 'mainnet' -server 'localhost:10009' -d -dx -pw 'lndg-admin' -dir /mnt/lnd"
-.venv/bin/pip install whitenoise tzdata && .venv/bin/python initialize.py -net 'mainnet' -server 'localhost:10009' -d -dx -pw 'lndg-admin' -dir /mnt/lnd
+echo "running .venv/bin/pip install whitenoise tzdata && .venv/bin/python initialize.py -net 'mainnet' -server 'localhost:10009' -d -dx -pw 'lndg-admin' -dir /mnt/lnd -ip 'hx4lftdn6wiyda5cp2atda6p5sdkusdsrkyifbmqgvwozmhpieo5jcid.local'"
+.venv/bin/pip install whitenoise tzdata && .venv/bin/python initialize.py -net 'mainnet' -server 'localhost:10009' -d -dx -pw 'lndg-admin' -dir /mnt/lnd -ip 'hx4lftdn6wiyda5cp2atda6p5sdkusdsrkyifbmqgvwozmhpieo5jcid.local'
 echo "running .venv/bin/python manage.py runserver 0.0.0.0:8889 "
 .venv/bin/python manage.py runserver 0.0.0.0:8889
-# echo "running .venv/bin/python jobs.py"
-# .venv/bin/python jobs.py

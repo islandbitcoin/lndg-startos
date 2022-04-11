0f02cdae9e6f0276bcf50f006c8d4a8abdc176db
Dread <dread@pop-os.localdomain>
2022-04-11 10:23:01 -0400

    updated ALLOWED_HOSTS

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -7,6 +7,7 @@ _term() {
 TOR_ADDRESS=$(yq e '.tor-address' /root/start9/config.yaml)
 LAN_ADDRESS=$(yq e '.lan-address' /root/start9/config.yaml)
 LND_ADDRESS='lnd.embassy'
+LNDG_ADDRESS='lndg.embassy'
 LNDG_PASS=$(yq e '.password' /root/start9/config.yaml)
 HOST_IP=$(ip -4 route list match 0/0 | awk '{print $3}')
 echo " \n Starting LNDg... \n"
@@ -18,7 +19,7 @@ CORS_ALLOW_CREDENTIALS = True
 GRPC_DNS_RESOLVER='native'
 CSRF_TRUSTED_ORIGINS = ['https://"$LAN_ADDRESS"']
 " >> lndg/settings.py
-sed -i "s/ALLOWED_HOSTS = \[/&'"$TOR_ADDRESS"','"$LND_ADDRESS"',/" lndg/settings.py
+sed -i "s/ALLOWED_HOSTS = \[/&'"$TOR_ADDRESS"','"$LNDG_ADDRESS"',/" lndg/settings.py
 sed -i "s/+ '\/data\/chain\/bitcoin\/' + LND_NETWORK +/ + /" /src/lndg/gui/lnd_deps/lnd_connect.py
 # Properties 
   echo 'version: 2' >> /root/start9/stats.yaml

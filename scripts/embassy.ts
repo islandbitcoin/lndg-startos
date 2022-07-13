286577592efa01b845d8376b36d61bbb6ca5ecea
Dread <dread@MacMax.lan>
2022-07-13 17:36:26 -0400

    removed dependencies.ts

--- scripts/embassy.ts
+++ scripts/embassy.ts
@@ -1,4 +1,3 @@
 export { setConfig } from "./services/setConfig.ts";
 export { properties } from "./services/properties.ts";
 export { getConfig } from "./services/getConfig.ts";
-export { dependencies } from "./services/dependencies.ts";

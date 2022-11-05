f074341904b422f9c5ff96c309520edea08cd76d
islandbitcoin <dread@start9.com>
2022-11-04 20:34:05 -0400

    update to x86_64 architecture

--- scripts/embassy.ts
+++ scripts/embassy.ts
@@ -1,4 +1,6 @@
-export { setConfig } from "./services/setConfig.ts";
-export { properties } from "./services/properties.ts";
-export { getConfig } from "./services/getConfig.ts";
-export { migration } from "./services/migrations.ts";
+export { setConfig } from "./procedures/setConfig.ts";
+export { properties } from "./procedures/properties.ts";
+export { getConfig } from "./procedures/getConfig.ts";
+export { migration } from "./procedures/migrations.ts";
+export { health } from "./procedures/healthChecks.ts";
+export { main } from "./procedures/main.ts";

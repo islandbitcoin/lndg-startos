909b0d6cd2f9d732f3bb23ed15f8a81c76600174
Matt Hill <mattnine@protonmail.com>
2025-05-21 21:35:24 -0600

    beta 24

--- startos/fileHelpers/lndg-admin.txt.ts
+++ startos/fileHelpers/lndg-admin.txt.ts
@@ -1,7 +1,10 @@
 import { matches, FileHelper } from '@start9labs/start-sdk'
 
 export const adminTxtFile = FileHelper.raw(
-  '/media/startos/volumes/app/data/lndg-admin.txt',
+  {
+    volumeId: 'data',
+    subpath: '/lndg-admin.txt',
+  },
   (a) => a,
   (a) => a,
   matches.string.unsafeCast,

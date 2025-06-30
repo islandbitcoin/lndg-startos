ee3914b3d2d6298b95d33dd6801e628237826456
Sirius <110729325+SiriusLightning21@users.noreply.github.com>
2025-06-30 15:02:38 +0200

    Aktualisieren von migrations.ts

--- scripts/procedures/migrations.ts
+++ scripts/procedures/migrations.ts
@@ -1,3 +1,3 @@
 import { compat, types as T } from "../deps.ts";
 
-export const migration: T.ExpectedExports.migration = compat.migrations.fromMapping({}, "1.9.1");
+export const migration: T.ExpectedExports.migration = compat.migrations.fromMapping({}, "1.10.1");
